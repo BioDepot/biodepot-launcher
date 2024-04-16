e664dadcd6f16a7abffb6fb22acb42da069ef696
Thomas Dahlstrom <tdahlstrom@gmail.com>
2024-04-16 13:43:21 -0700

    Found bug in sed command.

--- launcher-utils/launch.sh
+++ launcher-utils/launch.sh
@@ -60,7 +60,7 @@ ip=$(/workspace/docker-machine ip $instance_name)
 config_location="/root/.docker/machine/machines/$instance_name/config.json"
 
 if [ $os == "Windows" ]; then
-    win_home_dir=$(echo $home_dir | sed 's#\\#\/g')
+    win_home_dir=$(echo $home_dir | sed 's#\\#\/#g')
     sed -i 's#\"\/root\/#\"'"$win_home_dir"'\/#g' $config_location
     chmod -R 777 /root/.docker/machine/*
 else
