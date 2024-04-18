d94d57d1b536d8578cf601b6d604579f085adf27
Thomas Dahlstrom <tdahlstrom@gmail.com>
2024-04-18 14:14:06 -0700

    Updated utils image files.

--- launcher-utils/hash.sh
+++ launcher-utils/hash.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 dir=$1
 
-cmd="LC_ALL=C tar -C $dir -cf - --posix --pax-option=exthdr.name=%d/PaxHeaders/%f --pax-option=delete=atime,delete=ctime --mode='go+u,go-w' --sort=name --clamp-mtime --mtime='2015-10-21 00:00Z' --group=0 --owner=0 --numeric-owner . | sha256sum"
+cmd="tar -C $dir -cf - --posix --pax-option=exthdr.name=%d/PaxHeaders/%f --pax-option=delete=atime,delete=ctime --mode='600' --sort=name --mtime='2015-10-21 00:00Z' --group=0 --owner=0 --numeric-owner . | sha256sum"
 
 #echo $cmd
 eval $cmd | awk '{print $1}'
