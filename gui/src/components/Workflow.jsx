5b44bb3064083e9e11f167b06f90d43849b58244
Thomas Dahlstrom <tdahlstrom@gmail.com>
2024-04-18 14:46:59 -0700

    Check for proper creation of cmd.

--- gui/src/components/Workflow.jsx
+++ gui/src/components/Workflow.jsx
@@ -251,6 +251,7 @@ function Workflow(props) {
       if (window.NL_OS === "Windows") {
          let pwd = (await os.execCommand('echo %cd%')).stdOut;
          pwd = pwd.replace(/\\/g, '\/');
+         alert('docker run -v ' + pwd + ':/workspace/mnt biodepot/launcher-utils:1.0 "hash" /workspace/mnt/' + props.category + '/' + props.name);
          setHash((await os.execCommand('docker run -v ' + pwd + ':/workspace/mnt biodepot/launcher-utils:1.0 "hash" /workspace/mnt/' + props.category + '/' + props.name)).stdOut);
          alert(hash);
       } else {
